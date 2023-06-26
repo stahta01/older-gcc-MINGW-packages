db3389885d2462aca2a363514f353cefccfa7702
Tim Stahlhut <stahta01@gmail.com>
2023-06-26 10:19:53 -0400

    script: Edit copy-5.5.0-toolchain-packages.sh

--- copy-5.5.0-toolchain-packages.sh
+++ copy-5.5.0-toolchain-packages.sh
@@ -26,4 +26,4 @@ cp -f ./mingw-w64-crt7-git/mingw-w64-ucrt-x86_64-crt-git-7.0.0.r5.gb7f51b148-1-a
 cp -f ./mingw-w64-winpthreads7-git/mingw-w64-ucrt-x86_64-libwinpthread-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-winpthreads7-git/mingw-w64-ucrt-x86_64-winpthreads-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-gcc5-git/mingw-w64-ucrt-x86_64-gcc-libs-git-5.5.0+d20171010.r1.c16.g926d9947847a-2-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc5-git/mingw-ucrt-w64-x86_64-gcc-git-5.5.0+d20171010.r1.c16.g926d9947847a-2-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc5-git/mingw-w64-ucrt-x86_64-gcc-git-5.5.0+d20171010.r1.c16.g926d9947847a-2-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_UCRT64_packages/
