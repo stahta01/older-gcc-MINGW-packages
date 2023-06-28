1d8f28c1fc3b12fa4bacdc5575dfc614bd04ff03
Tim Stahlhut <stahta01@gmail.com>
2023-06-28 15:22:15 -0400

    script: Edit copy-6.4.0-toolchain-packages.sh
    Copy CRT10 for UCRT

--- copy-6.4.0-toolchain-packages.sh
+++ copy-6.4.0-toolchain-packages.sh
@@ -21,9 +21,9 @@ cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc-6.4.0-1-any.pkg.tar.zst ./packages/gcc
 mkdir -p packages/gcc6.4.0_toolchain_mingw_UCRT64_packages
 
 rm -f ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/*.zst
-cp -f ./mingw-w64-headers7-git/mingw-w64-ucrt-x86_64-headers-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-crt7-git/mingw-w64-ucrt-x86_64-crt-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-winpthreads7-git/mingw-w64-ucrt-x86_64-libwinpthread-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-winpthreads7-git/mingw-w64-ucrt-x86_64-winpthreads-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-headers10-git/mingw-w64-ucrt-x86_64-headers-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-crt10-git/mingw-w64-ucrt-x86_64-crt-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-ucrt-x86_64-libwinpthread-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-ucrt-x86_64-winpthreads-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc-libs-6.4.0-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-ucrt-x86_64-gcc-6.4.0-1-any.pkg.tar.zst ./packages/gcc6.4.0_toolchain_mingw_UCRT64_packages/
