26efbe765807f69cd41920de8276abb45d7af787
Tim Stahlhut <stahta01@gmail.com>
2023-06-30 17:52:25 -0400

    script: Add copy-11.4.0-toolchain-packages.sh

---
+++ copy-11.4.0-toolchain-packages.sh
@@ -0,0 +1,29 @@
+mkdir -p packages/gcc11.4.0_toolchain_mingw_64_packages
+
+rm -f ./packages/gcc11.4.0_toolchain_mingw_64_packages/*.zst
+cp -f ./mingw-w64-headers10-git/mingw-w64-x86_64-headers-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-crt10-git/mingw-w64-x86_64-crt-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-x86_64-libwinpthread-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-x86_64-winpthreads-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc11/mingw-w64-x86_64-gcc-libs-11.4.0-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc11/mingw-w64-x86_64-gcc-11.4.0-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_64_packages/
+
+mkdir -p packages/gcc11.4.0_toolchain_mingw_32_packages
+
+rm -f ./packages/gcc11.4.0_toolchain_mingw_32_packages/*.zst
+cp -f ./mingw-w64-headers10-git/mingw-w64-i686-headers-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt10-git/mingw-w64-i686-crt-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-i686-libwinpthread-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-i686-winpthreads-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc11/mingw-w64-i686-gcc-libs-11.4.0-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc11/mingw-w64-i686-gcc-11.4.0-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_32_packages/
+
+mkdir -p packages/gcc11.4.0_toolchain_mingw_UCRT64_packages
+
+rm -f ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/*.zst
+cp -f ./mingw-w64-headers10-git/mingw-w64-ucrt-x86_64-headers-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-crt10-git/mingw-w64-ucrt-x86_64-crt-git-10.0.0.r184.gab72a4850-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-ucrt-x86_64-libwinpthread-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-winpthreads10-git/mingw-w64-ucrt-x86_64-winpthreads-git-10.0.0.r258.g530c58e17-2-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc11/mingw-w64-ucrt-x86_64-gcc-libs-11.4.0-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc11/mingw-w64-ucrt-x86_64-gcc-11.4.0-1-any.pkg.tar.zst ./packages/gcc11.4.0_toolchain_mingw_UCRT64_packages/
