f28d52b34039e6df72a64ef1addb52ca5689d3ed
Tim Stahlhut <stahta01@gmail.com>
2023-07-06 20:09:17 -0400

    script: Add fetch-gcc.sh

---
+++ fetch-gcc.sh
@@ -0,0 +1,21 @@
+cd gcc-git && \
+git fetch origin releases/gcc-3.4 && \
+git fetch origin releases/gcc-4.0 && \
+git fetch origin releases/gcc-4.1 && \
+git fetch origin releases/gcc-4.2 && \
+git fetch origin releases/gcc-4.3 && \
+git fetch origin releases/gcc-4.4 && \
+git fetch origin releases/gcc-4.5 && \
+git fetch origin releases/gcc-4.6 && \
+git fetch origin releases/gcc-4.7 && \
+git fetch origin releases/gcc-4.8 && \
+git fetch origin releases/gcc-4.9 && \
+git fetch origin releases/gcc-5 && \
+git fetch origin releases/gcc-6 && \
+git fetch origin releases/gcc-7 && \
+git fetch origin releases/gcc-8 && \
+git fetch origin releases/gcc-9 && \
+git fetch origin releases/gcc-10 && \
+git fetch origin releases/gcc-11 && \
+git fetch origin releases/gcc-12 && \
+git fetch origin master
