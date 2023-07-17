2caf974bce212aacb8b0f13c37c98b5874f8d874
Tim Stahlhut <stahta01@gmail.com>
2023-07-16 20:59:29 -0400

    script: Add copy-4.5.4-toolchain-packages.sh

---
+++ copy-4.5.4-toolchain-packages.sh
@@ -0,0 +1,7 @@
+mkdir -p packages/gcc4.5.4_toolchain_mingw_32_packages
+
+rm -f ./packages/gcc4.5.4_toolchain_mingw_32_packages/*.zst
+cp -f ./mingw-w64-crt2-git/mingw-w64-i686-headers-git-2.0.6.r0.g06c5a4241-2-any.pkg.tar.zst ./packages/gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt2-git/mingw-w64-i686-crt-git-2.0.6.r0.g06c5a4241-2-any.pkg.tar.zst ./packages/gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc4.5-git/mingw-w64-i686-gcc-git-4.5.4+d20120702.r42.c7.g41eccc86828f-2-any.pkg.tar.zst ./packages/gcc4.5.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc4.5-git/mingw-w64-i686-gcc-libs-git-4.5.4+d20120702.r42.c7.g41eccc86828f-2-any.pkg.tar.zst ./packages/gcc4.5.4_toolchain_mingw_32_packages/
