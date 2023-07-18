c50d45a070882c281423022300c6c096cce6a3c7
Tim Stahlhut <stahta01@gmail.com>
2023-07-17 20:34:30 -0400

    script: Edit copy-5.5.0-toolchain-packages.sh copy Ada and CRT 3 for MINGW32

--- copy-5.5.0-toolchain-packages.sh
+++ copy-5.5.0-toolchain-packages.sh
@@ -11,12 +11,13 @@ cp -f ./mingw-w64-gcc5-git/mingw-w64-x86_64-gcc-git-5.5.0+d20171010.r1.c17.g926d
 mkdir -p packages/gcc5.5.0_toolchain_mingw_32_packages
 
 rm -f ./packages/gcc5.5.0_toolchain_mingw_32_packages/*.zst
-cp -f ./mingw-w64-headers6-git/mingw-w64-i686-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-crt6-git/mingw-w64-i686-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-winpthreads6-git/mingw-w64-i686-libwinpthread-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-winpthreads6-git/mingw-w64-i686-winpthreads-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-headers3-git/mingw-w64-i686-headers-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt3-git/mingw-w64-i686-crt-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-i686-libwinpthread-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-i686-winpthreads-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc5-git/mingw-w64-i686-gcc-libs-git-5.5.0+d20171010.r1.c17.g926d9947847a-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc5-git/mingw-w64-i686-gcc-git-5.5.0+d20171010.r1.c17.g926d9947847a-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc5-git/mingw-w64-i686-gcc-ada-git-5.5.0+d20171010.r1.c17.g926d9947847a-1-any.pkg.tar.zst ./packages/gcc5.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc5.5.0_toolchain_mingw_UCRT64_packages
 
