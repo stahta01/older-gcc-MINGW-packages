7617a708505b7117aaa1df3f563138abbb10a836
Tim Stahlhut <stahta01@gmail.com>
2023-07-20 18:20:27 -0400

    script: Edit copy-6.5.0-toolchain-packages.sh use crt5 and copy Ada for MINGW32

--- copy-6.5.0-toolchain-packages.sh
+++ copy-6.5.0-toolchain-packages.sh
@@ -11,12 +11,13 @@ cp -f ./mingw-w64-gcc6/mingw-w64-x86_64-gcc-6.5.0-1-any.pkg.tar.zst ./packages/g
 mkdir -p packages/gcc6.5.0_toolchain_mingw_32_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_32_packages/*.zst
-cp -f ./mingw-w64-headers6-git/mingw-w64-i686-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-crt6-git/mingw-w64-i686-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-winpthreads6-git/mingw-w64-i686-libwinpthread-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-winpthreads6-git/mingw-w64-i686-winpthreads-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-headers5-git/mingw-w64-i686-headers-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt5-git/mingw-w64-i686-crt-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads5-git/mingw-w64-i686-libwinpthread-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads5-git/mingw-w64-i686-winpthreads-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc-libs-6.5.0-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc-6.5.0-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc6/mingw-w64-i686-gcc-ada-6.5.0-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc6.5.0_toolchain_mingw_UCRT64_packages
 
