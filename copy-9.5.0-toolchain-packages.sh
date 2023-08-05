3e451f7005704bd50675c3caa7c1733b2f9bd40f
Tim Stahlhut <stahta01@gmail.com>
2023-08-05 06:38:36 -0400

    script: Update copy-9.5.0-toolchain-packages.sh
    Add copy Ada for mingw32 and mingw64.

--- copy-9.5.0-toolchain-packages.sh
+++ copy-9.5.0-toolchain-packages.sh
@@ -7,6 +7,7 @@ cp -f ./mingw-w64-winpthreads8-git/mingw-w64-x86_64-libwinpthread-git-8.0.2.r0.g
 cp -f ./mingw-w64-winpthreads8-git/mingw-w64-x86_64-winpthreads-git-8.0.2.r0.g368175526-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc9-git/mingw-w64-x86_64-gcc-libs-git-9.5.0+d20220527.r2.c11.g7a15b5060a83-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc9-git/mingw-w64-x86_64-gcc-git-9.5.0+d20220527.r2.c11.g7a15b5060a83-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc9-git/mingw-w64-x86_64-gcc-ada-git-9.5.0+d20220527.r2.c11.g7a15b5060a83-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_64_packages/
 
 mkdir -p packages/gcc9.5.0_toolchain_mingw_32_packages
 
@@ -17,6 +18,7 @@ cp -f ./mingw-w64-winpthreads8-git/mingw-w64-i686-libwinpthread-git-8.0.2.r0.g36
 cp -f ./mingw-w64-winpthreads8-git/mingw-w64-i686-winpthreads-git-8.0.2.r0.g368175526-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc9-git/mingw-w64-i686-gcc-libs-git-9.5.0+d20220527.r2.c11.g7a15b5060a83-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc9-git/mingw-w64-i686-gcc-git-9.5.0+d20220527.r2.c11.g7a15b5060a83-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc9-git/mingw-w64-i686-gcc-ada-git-9.5.0+d20220527.r2.c11.g7a15b5060a83-1-any.pkg.tar.zst ./packages/gcc9.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc9.5.0_toolchain_mingw_UCRT64_packages
 
