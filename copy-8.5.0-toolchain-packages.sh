5a3648f290f108f62ddd0a9d638d69ff78a5c96f
Tim Stahlhut <stahta01@gmail.com>
2023-08-05 06:38:34 -0400

    script: Update copy-8.5.0-toolchain-packages.sh by
    Add copy Ada for mingw32 and mingw64

--- copy-8.5.0-toolchain-packages.sh
+++ copy-8.5.0-toolchain-packages.sh
@@ -7,6 +7,7 @@ cp -f ./mingw-w64-winpthreads7-git/mingw-w64-x86_64-libwinpthread-git-7.0.0.r5.g
 cp -f ./mingw-w64-winpthreads7-git/mingw-w64-x86_64-winpthreads-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc8-git/mingw-w64-x86_64-gcc-libs-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc8-git/mingw-w64-x86_64-gcc-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc8-git/mingw-w64-x86_64-gcc-ada-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_64_packages/
 
 mkdir -p packages/gcc8.5.0_toolchain_mingw_32_packages
 
@@ -17,6 +18,7 @@ cp -f ./mingw-w64-winpthreads7-git/mingw-w64-i686-libwinpthread-git-7.0.0.r5.gb7
 cp -f ./mingw-w64-winpthreads7-git/mingw-w64-i686-winpthreads-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc8-git/mingw-w64-i686-gcc-libs-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc8-git/mingw-w64-i686-gcc-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc8-git/mingw-w64-i686-gcc-ada-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc8.5.0_toolchain_mingw_UCRT64_packages
 
@@ -27,3 +29,4 @@ cp -f ./mingw-w64-winpthreads7-git/mingw-w64-ucrt-x86_64-libwinpthread-git-7.0.0
 cp -f ./mingw-w64-winpthreads7-git/mingw-w64-ucrt-x86_64-winpthreads-git-7.0.0.r5.gb7f51b148-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-gcc8-git/mingw-w64-ucrt-x86_64-gcc-libs-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-gcc8-git/mingw-w64-ucrt-x86_64-gcc-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_UCRT64_packages/
+#cp -f ./mingw-w64-gcc8-git/mingw-w64-ucrt-x86_64-gcc-ada-git-8.5.0+d20210514.r1.c12.geafe83f2f20e-1-any.pkg.tar.zst ./packages/gcc8.5.0_toolchain_mingw_UCRT64_packages/
