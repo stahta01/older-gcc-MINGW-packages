ae4c6a2d7e44230d283b225ee5014a5800ad0d4d
Tim Stahlhut <stahta01@gmail.com>
2023-08-07 01:48:58 -0400

    script: Update copy-7.5.0-toolchain-packages.sh for Ada
    and GCC pkgrel=2

--- copy-7.5.0-toolchain-packages.sh
+++ copy-7.5.0-toolchain-packages.sh
@@ -5,8 +5,9 @@ cp -f ./mingw-w64-headers6-git/mingw-w64-x86_64-headers-git-6.0.0.r56.gd72c4fe62
 cp -f ./mingw-w64-crt6-git/mingw-w64-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-winpthreads6-git/mingw-w64-x86_64-libwinpthread-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-winpthreads6-git/mingw-w64-x86_64-winpthreads-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc7-git/mingw-w64-x86_64-gcc-libs-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc7-git/mingw-w64-x86_64-gcc-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-x86_64-gcc-libs-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-x86_64-gcc-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-x86_64-gcc-ada-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_64_packages/
 
 mkdir -p packages/gcc7.5.0_toolchain_mingw_32_packages
 
@@ -15,8 +16,9 @@ cp -f ./mingw-w64-headers6-git/mingw-w64-i686-headers-git-6.0.0.r56.gd72c4fe62-1
 cp -f ./mingw-w64-crt6-git/mingw-w64-i686-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-winpthreads6-git/mingw-w64-i686-libwinpthread-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-winpthreads6-git/mingw-w64-i686-winpthreads-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc7-git/mingw-w64-i686-gcc-libs-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc7-git/mingw-w64-i686-gcc-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-i686-gcc-libs-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-i686-gcc-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-i686-gcc-ada-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc7.5.0_toolchain_mingw_UCRT64_packages
 
@@ -25,5 +27,6 @@ cp -f ./mingw-w64-headers6-git/mingw-w64-ucrt-x86_64-headers-git-6.0.0.r56.gd72c
 cp -f ./mingw-w64-crt6-git/mingw-w64-ucrt-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-winpthreads6-git/mingw-w64-ucrt-x86_64-libwinpthread-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-winpthreads6-git/mingw-w64-ucrt-x86_64-winpthreads-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc7-git/mingw-w64-ucrt-x86_64-gcc-libs-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc7-git/mingw-w64-ucrt-x86_64-gcc-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-1-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-ucrt-x86_64-gcc-libs-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-ucrt-x86_64-gcc-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc7-git/mingw-w64-ucrt-x86_64-gcc-ada-git-7.5.0+d20191114.r1.c15.gb2d961e7342b-2-any.pkg.tar.zst ./packages/gcc7.5.0_toolchain_mingw_UCRT64_packages/
