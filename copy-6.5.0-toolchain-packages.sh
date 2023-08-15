704bda72acf36c4c81a88f45a2f603a14aeb21c6
Tim Stahlhut <stahta01@gmail.com>
2023-08-14 20:09:47 -0400

    script: Update copy-6.5.0-toolchain-packages.sh

--- copy-6.5.0-toolchain-packages.sh
+++ copy-6.5.0-toolchain-packages.sh
@@ -1,10 +1,10 @@
 mkdir -p packages/gcc6.5.0_toolchain_mingw_64_packages
 
 rm -f ./packages/gcc6.5.0_toolchain_mingw_64_packages/*.zst
-cp -f ./mingw-w64-headers6-git/mingw-w64-x86_64-headers-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-crt6-git/mingw-w64-x86_64-crt-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-winpthreads6-git/mingw-w64-x86_64-libwinpthread-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-winpthreads6-git/mingw-w64-x86_64-winpthreads-git-6.0.0.r56.gd72c4fe62-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-headers5-git/mingw-w64-x86_64-headers-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-crt5-git/mingw-w64-x86_64-crt-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads5-git/mingw-w64-x86_64-libwinpthread-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads5-git/mingw-w64-x86_64-winpthreads-git-5.0.4.r16.g715ecca90-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc6-git/mingw-w64-x86_64-gcc-libs-git-6.5.0+d20181026.r2.c13.g39a300fd08a8-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc6-git/mingw-w64-x86_64-gcc-git-6.5.0+d20181026.r2.c13.g39a300fd08a8-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-gcc6-git/mingw-w64-x86_64-gcc-ada-git-6.5.0+d20181026.r2.c13.g39a300fd08a8-1-any.pkg.tar.zst ./packages/gcc6.5.0_toolchain_mingw_64_packages/
