85a14a8632aa1179c5bdb41715cfdfe52f5f168a
Tim Stahlhut <stahta01@gmail.com>
2023-08-16 10:46:59 -0400

    script: Update copy-4.7.4-toolchain-packages.sh

--- copy-4.7.4-toolchain-packages.sh
+++ copy-4.7.4-toolchain-packages.sh
@@ -1,12 +1,13 @@
-#mkdir -p packages/gcc4.7.4_toolchain_mingw_64_packages
+mkdir -p packages/gcc4.7.4_toolchain_mingw_64_packages
 
-#rm -f ./packages/gcc4.7.4_toolchain_mingw_64_packages/*.zst
-#cp -f ./mingw-w64-headers6-git/mingw-w64-x86_64-headers-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
-#cp -f ./mingw-w64-crt6-git/mingw-w64-x86_64-crt-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
-#cp -f ./mingw-w64-winpthreads6-git/mingw-w64-x86_64-libwinpthread-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
-#cp -f ./mingw-w64-winpthreads6-git/mingw-w64-x86_64-winpthreads-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
-#cp -f ./mingw-w64-gcc4.7-git/mingw-w64-x86_64-gcc-libs-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
-#cp -f ./mingw-w64-gcc4.7-git/mingw-w64-x86_64-gcc-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+rm -f ./packages/gcc4.7.4_toolchain_mingw_64_packages/*.zst
+cp -f ./mingw-w64-headers3-git/mingw-w64-x86_64-headers-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-crt3-git/mingw-w64-x86_64-crt-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-x86_64-libwinpthread-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-x86_64-winpthreads-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc4.7-git/mingw-w64-x86_64-gcc-libs-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc4.7-git/mingw-w64-x86_64-gcc-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc4.7-git/mingw-w64-x86_64-gcc-ada-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_64_packages/
 
 mkdir -p packages/gcc4.7.4_toolchain_mingw_32_packages
 
@@ -18,13 +19,3 @@ cp -f ./mingw-w64-winpthreads3-git/mingw-w64-i686-winpthreads-git-3.3.0.r61.g8c7
 cp -f ./mingw-w64-gcc4.7-git/mingw-w64-i686-gcc-libs-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc4.7-git/mingw-w64-i686-gcc-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc4.7-git/mingw-w64-i686-gcc-ada-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_32_packages/
-
-#mkdir -p packages/gcc4.7.4_toolchain_mingw_UCRT64_packages
-
-#rm -f ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/*.zst
-#cp -f ./mingw-w64-headers6-git/mingw-w64-ucrt-x86_64-headers-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/
-#cp -f ./mingw-w64-crt6-git/mingw-w64-ucrt-x86_64-crt-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/
-#cp -f ./mingw-w64-winpthreads6-git/mingw-w64-ucrt-x86_64-libwinpthread-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/
-#cp -f ./mingw-w64-winpthreads6-git/mingw-w64-ucrt-x86_64-winpthreads-git-3.3.0.r61.g8c7e00886-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/
-#cp -f ./mingw-w64-gcc4.7-git/mingw-w64-ucrt-x86_64-gcc-libs-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/
-#cp -f ./mingw-w64-gcc4.7-git/mingw-w64-ucrt-x86_64-gcc-git-4.7.4+d20140612.r45.c10.g4d1f511d7f2f-1-any.pkg.tar.zst ./packages/gcc4.7.4_toolchain_mingw_UCRT64_packages/
