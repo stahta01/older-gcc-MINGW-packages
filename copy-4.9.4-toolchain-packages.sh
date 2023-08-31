709b0229807f3f548f16d8d81b613034d33da271
Tim Stahlhut <stahta01@gmail.com>
2023-08-31 10:44:55 -0400

    script: Add copy-4.9.4-toolchain-packages.sh

---
+++ copy-4.9.4-toolchain-packages.sh
@@ -0,0 +1,21 @@
+mkdir -p packages/gcc4.9.4_toolchain_mingw_64_packages
+
+rm -f ./packages/gcc4.9.4_toolchain_mingw_64_packages/*.zst
+cp -f ./mingw-w64-headers3-git/mingw-w64-x86_64-headers-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-crt3-git/mingw-w64-x86_64-crt-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-x86_64-libwinpthread-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-x86_64-winpthreads-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc4.9-git/mingw-w64-x86_64-gcc-libs-git-4.9.4+d20160726.r51.c1.g4f18db57daff-1-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc4.9-git/mingw-w64-x86_64-gcc-git-4.9.4+d20160726.r51.c1.g4f18db57daff-1-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc4.9-git/mingw-w64-x86_64-gcc-ada-git-4.9.4+d20160726.r51.c1.g4f18db57daff-1-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_64_packages/
+
+mkdir -p packages/gcc4.9.4_toolchain_mingw_32_packages
+
+rm -f ./packages/gcc4.9.4_toolchain_mingw_32_packages/*.zst
+cp -f ./mingw-w64-headers3-git/mingw-w64-i686-headers-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt3-git/mingw-w64-i686-crt-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-i686-libwinpthread-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-winpthreads3-git/mingw-w64-i686-winpthreads-git-3.3.0.r61.g8c7e00886-3-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc4.9-git/mingw-w64-i686-gcc-libs-git-4.9.4+d20160726.r51.c1.g4f18db57daff-1-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc4.9-git/mingw-w64-i686-gcc-git-4.9.4+d20160726.r51.c1.g4f18db57daff-1-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc4.9-git/mingw-w64-i686-gcc-ada-git-4.9.4+d20160726.r51.c1.g4f18db57daff-1-any.pkg.tar.zst ./packages/gcc4.9.4_toolchain_mingw_32_packages/
