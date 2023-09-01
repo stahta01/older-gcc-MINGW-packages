8b2230a84b4945d94e085e7e92565accbcd8d11b
Tim Stahlhut <stahta01@gmail.com>
2023-08-31 22:24:19 -0400

    script: Update copy-4.2.5-toolchain-packages.sh

--- copy-4.2.5-toolchain-packages.sh
+++ copy-4.2.5-toolchain-packages.sh
@@ -1,6 +1,6 @@
 mkdir -p packages/gcc4.2.5_toolchain_mingw_32_packages
 
 rm -f ./packages/gcc4.2.5_toolchain_mingw_32_packages/*.zst
-cp -f ./mingw-w64-crt2-git/mingw-w64-i686-headers-git-2.0.6.r0.g06c5a4241-2-any.pkg.tar.zst ./packages/gcc4.2.5_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-crt2-git/mingw-w64-i686-crt-git-2.0.6.r0.g06c5a4241-2-any.pkg.tar.zst ./packages/gcc4.2.5_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt2-git/mingw-w64-i686-headers-git-2.0.9.r0.gea32a0c5b-2-any.pkg.tar.zst ./packages/gcc4.2.5_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-crt2-git/mingw-w64-i686-crt-git-2.0.9.r0.gea32a0c5b-2-any.pkg.tar.zst ./packages/gcc4.2.5_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-gcc4.2-git/mingw-w64-i686-gcc-git-4.2.5+d20090330.r0.c3.g87bf6ca58e51-2-any.pkg.tar.zst ./packages/gcc4.2.5_toolchain_mingw_32_packages/
