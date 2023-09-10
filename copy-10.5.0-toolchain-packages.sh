0754e203385fde8aeaa23396be8d558d37702bc6
Tim Stahlhut <stahta01@gmail.com>
2023-09-10 12:03:53 -0400

    script: Update copy-10.5.0-toolchain-packages.sh

--- copy-10.5.0-toolchain-packages.sh
+++ copy-10.5.0-toolchain-packages.sh
@@ -5,9 +5,9 @@ cp -f ./mingw-w64-headers9-git/mingw-w64-x86_64-headers-git-9.0.0.r32.gae63cde27
 cp -f ./mingw-w64-crt9-git/mingw-w64-x86_64-crt-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-winpthreads9-git/mingw-w64-x86_64-libwinpthread-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
 cp -f ./mingw-w64-winpthreads9-git/mingw-w64-x86_64-winpthreads-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-x86_64-gcc-libs-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-x86_64-gcc-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-x86_64-gcc-ada-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-x86_64-gcc-libs-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-x86_64-gcc-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-x86_64-gcc-ada-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_64_packages/
 
 mkdir -p packages/gcc10.5.0_toolchain_mingw_32_packages
 
@@ -16,9 +16,9 @@ cp -f ./mingw-w64-headers9-git/mingw-w64-i686-headers-git-9.0.0.r32.gae63cde27-1
 cp -f ./mingw-w64-crt9-git/mingw-w64-i686-crt-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-winpthreads9-git/mingw-w64-i686-libwinpthread-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
 cp -f ./mingw-w64-winpthreads9-git/mingw-w64-i686-winpthreads-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-i686-gcc-libs-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-i686-gcc-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-i686-gcc-ada-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-i686-gcc-libs-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-i686-gcc-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-i686-gcc-ada-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_32_packages/
 
 mkdir -p packages/gcc10.5.0_toolchain_mingw_UCRT64_packages
 
@@ -27,6 +27,6 @@ cp -f ./mingw-w64-headers9-git/mingw-w64-ucrt-x86_64-headers-git-9.0.0.r32.gae63
 cp -f ./mingw-w64-crt9-git/mingw-w64-ucrt-x86_64-crt-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-winpthreads9-git/mingw-w64-ucrt-x86_64-libwinpthread-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
 cp -f ./mingw-w64-winpthreads9-git/mingw-w64-ucrt-x86_64-winpthreads-git-9.0.0.r32.gae63cde27-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-ucrt-x86_64-gcc-libs-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-ucrt-x86_64-gcc-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
-cp -f ./mingw-w64-gcc10-git/mingw-w64-ucrt-x86_64-gcc-ada-git-10.5.0+d20230707.r1.c1.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-ucrt-x86_64-gcc-libs-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-ucrt-x86_64-gcc-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
+cp -f ./mingw-w64-gcc10-git/mingw-w64-ucrt-x86_64-gcc-ada-git-10.5.0+d20230707.r1.c2.gd04fe5541c53-1-any.pkg.tar.zst ./packages/gcc10.5.0_toolchain_mingw_UCRT64_packages/
