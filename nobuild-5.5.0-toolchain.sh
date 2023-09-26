67b161c3347ca123f2d238034c24fec7a97d646b
Tim Stahlhut <stahta01@gmail.com>
2023-09-26 16:36:16 -0400

    script: Update nobuild-5.5.0-toolchain.sh

--- nobuild-5.5.0-toolchain.sh
+++ nobuild-5.5.0-toolchain.sh
@@ -6,9 +6,9 @@ fi
 (
   cd mingw-w64-headers4-git && \
   MINGW_ARCH=mingw64 makepkg-mingw -s --holdver --nobuild && \
-  cd mingw-w64-headers7-git && \
+  cd ../mingw-w64-headers7-git && \
   MINGW_ARCH=mingw64 makepkg-mingw -s --holdver --nobuild && \
-  cd mingw-w64-gcc5-git && \
+  cd ../mingw-w64-gcc5-git && \
   MINGW_ARCH=mingw64 makepkg-mingw -s --holdver --nobuild && \
   echo "Script worked"
 ) || \
