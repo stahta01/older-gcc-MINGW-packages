b16781c7202e074bf32b404a684509eaac3ac304
Tim Stahlhut <stahta01@gmail.com>
2023-09-26 16:05:44 -0400

    script: Add rebuild-5.5.0-toolchain.sh

---
+++ rebuild-5.5.0-toolchain.sh
@@ -0,0 +1,18 @@
+if [[ "$MSYSTEM" != "MSYS" ]]; then
+  echo "This script is designed to be ran from MSYS console"
+  exit 1
+fi
+
+(
+  cd mingw-w64-headers4-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd mingw-w64-headers7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-gcc5-git && \
+  MINGW_ARCH='mingw64 mingw32 ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  echo "Script worked"
+) || \
+echo "Script failed"
