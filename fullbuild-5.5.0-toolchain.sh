7548a71602bf1341b08f4a617c283f4680af6c91
Tim Stahlhut <stahta01@gmail.com>
2023-09-26 18:11:18 -0400

    script: Update fullbuild-5.5.0-toolchain.sh

--- fullbuild-5.5.0-toolchain.sh
+++ fullbuild-5.5.0-toolchain.sh
@@ -4,13 +4,25 @@ if [[ "$MSYSTEM" != "MSYS" ]]; then
 fi
 
 (
-  cd ../mingw-w64-gcc5-git && \
+  cd mingw-w64-gcc5-git && \
   MINGW_ARCH='mingw64 mingw32 ucrt64' \
   makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
-  cd mingw-w64-headers4-git && \
+  cd ../mingw-w64-headers4-git && \
   MINGW_ARCH='mingw64 mingw32' \
   makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
-  cd mingw-w64-headers7-git && \
+  cd ../mingw-w64-headers7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-crt4-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-crt7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-winpthreads4-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-winpthreads7-git && \
   MINGW_ARCH='ucrt64' \
   makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
   cd ../mingw-w64-gcc5-git && \
