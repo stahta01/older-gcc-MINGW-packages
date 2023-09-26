81f03af88b75ccd963ed8036772bbef9b8497150
Tim Stahlhut <stahta01@gmail.com>
2023-09-26 16:05:44 -0400

    script: Add nobuild-5.5.0-toolchain.sh

---
+++ nobuild-5.5.0-toolchain.sh
@@ -0,0 +1,15 @@
+if [[ "$MSYSTEM" != "MSYS" ]]; then
+  echo "This script is designed to be ran from MSYS console"
+  exit 1
+fi
+
+(
+  cd mingw-w64-headers4-git && \
+  MINGW_ARCH=mingw64 makepkg-mingw -s --holdver --nobuild && \
+  cd mingw-w64-headers7-git && \
+  MINGW_ARCH=mingw64 makepkg-mingw -s --holdver --nobuild && \
+  cd mingw-w64-gcc5-git && \
+  MINGW_ARCH=mingw64 makepkg-mingw -s --holdver --nobuild && \
+  echo "Script worked"
+) || \
+echo "Script failed"
