6c1b9ac835d68596ea1ce0f0a5d5f4f333ee042e
Tim Stahlhut <stahta01@gmail.com>
2023-09-26 17:38:12 -0400

    script: Update rebuild-5.5.0-toolchain.sh

--- rebuild-5.5.0-toolchain.sh
+++ rebuild-5.5.0-toolchain.sh
@@ -7,7 +7,19 @@ fi
   cd mingw-w64-headers4-git && \
   MINGW_ARCH='mingw64 mingw32' \
   makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
-  cd mingw-w64-headers7-git && \
+  cd ../mingw-w64-headers7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-crt4-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-crt7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-winpthreads4-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-winpthreads7-git && \
   MINGW_ARCH='ucrt64' \
   makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
   cd ../mingw-w64-gcc5-git && \
