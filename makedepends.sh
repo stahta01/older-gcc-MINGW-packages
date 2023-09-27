56ce8b891179fc960746f837065b6f7d33be4177
Tim Stahlhut <stahta01@gmail.com>
2023-09-27 16:05:15 -0400

    script: Add makedepends.sh

---
+++ makedepends.sh
@@ -0,0 +1,9 @@
+pacman -S --needed base-devel && \
+pacman -S --needed autotools && \
+pacman -S --needed mingw-w64-x86_64-tools && \
+pacman -S --needed mingw-w64-i686-tools && \
+pacman -S --needed mingw-w64-ucrt-x86_64-tools && \
+pacman -S --needed mingw-w64-x86_64-isl && \
+pacman -S --needed mingw-w64-i686-isl && \
+pacman -S --needed mingw-w64-ucrt-x86_64-isl && \
+echo "Script worked"
