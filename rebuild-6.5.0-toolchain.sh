bc037c2dd9075bdd1b32f4253b9ba0a42a9c8810
Tim Stahlhut <stahta01@gmail.com>
2023-09-27 16:05:14 -0400

    script: Add rebuild-6.5.0-toolchain.sh

---
+++ rebuild-6.5.0-toolchain.sh
@@ -0,0 +1,30 @@
+if [[ "$MSYSTEM" != "MSYS" ]]; then
+  echo "This script is designed to be ran from MSYS console"
+  exit 1
+fi
+
+(
+  cd mingw-w64-headers5-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-headers7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-crt5-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-crt7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-winpthreads5-git && \
+  MINGW_ARCH='mingw64 mingw32' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-winpthreads7-git && \
+  MINGW_ARCH='ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  cd ../mingw-w64-gcc6-git && \
+  MINGW_ARCH='mingw64 mingw32 ucrt64' \
+  makepkg-mingw -Lf --install --noextract --holdver --noconfirm && \
+  echo "Script worked"
+) || \
+echo "Script failed"
