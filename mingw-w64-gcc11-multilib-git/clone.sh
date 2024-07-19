48f5d1fccf16ccc017f10741618d754b0beeb694
Tim Stahlhut <stahta01@gmail.com>
2024-07-19 11:15:51 -0400

    mingw-w64-gcc11-multilib-git: Add package

---
+++ mingw-w64-gcc11-multilib-git/clone.sh
@@ -0,0 +1,5 @@
+git clone --bare --mirror --dissociate --reference-if-able ../gcc-git \
+  --no-tags --single-branch --branch releases/gcc-11 --progress \
+  -- https://github.com/gcc-mirror/gcc.git gcc-git && \
+cd gcc-git && \
+git config remote.origin.fetch +refs/heads/releases/gcc-11:refs/heads/releases/gcc-11
